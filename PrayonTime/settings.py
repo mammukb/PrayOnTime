4d5a34ba515e1f3f10f0d16d7595b2997685a4d1
mammukb <muhammedkb156@gmail.com>
2024-05-27 23:36:54 +0530

    more filess

--- PrayonTime/settings.py
+++ PrayonTime/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[BASE_DIR,'static/']
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/')
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
